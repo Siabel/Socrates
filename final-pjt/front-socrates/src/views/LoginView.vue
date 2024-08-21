3be8b6f31ff9dc4483db01e7428db0f6208e4625
정원종 <jwj970802@gmail.com>
2024-08-22 02:59:37 +0900

    init: Socrates project

---
+++ final-pjt/front-socrates/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <form @submit.prevent="logIn">
+            <input type="text" v-model.trim="username">
+            <input type="password" v-model.trim="password">
+            <input type="submit">
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
