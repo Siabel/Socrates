bc4be8778ec320d61a5e380ff9436e4c8914e527
정원종 <jwj970802@gmail.com>
2025-05-23 12:48:55 +0900

    delete : KEY delete

--- front-socrates/src/stores/movie.js
+++ front-socrates/src/stores/movie.js
@@ -10,7 +10,6 @@ export const useMovieStore = defineStore('movies', () => {
   const genres = ref([])
   const comments = ref([])
   const query = ref('')
-  const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTI5YzI3MDdiZGNjODg3ODBiNDI2Mzg3ZDBhMWM4OSIsInN1YiI6IjY1M2IwNmU0NzE5YWViMDBjNDg5NDBlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3TDMrUHoP81gjcte2F_JsA776OcS1kGmA0gNLEhGx4k'
 
   // 모든 장르 가지고 오기
   const getGenres = function () {
